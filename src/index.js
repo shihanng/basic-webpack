65a172b5e9b676c5f7314352dc23f43e605bb8f6
Shi Han NG <shihanng@gmail.com>
2021-01-13 21:48:40 +0900

    Add button.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import makeButton from "./button";
 import { bottom, top } from "./footer";
 import nav from "./nav";
 
 console.log(nav);
 console.log(top, bottom);
+console.log(makeButton("simple"));
