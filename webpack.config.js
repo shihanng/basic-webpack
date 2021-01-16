f875546c332dd9995d63ff486e0dcdad0334f021
Shi Han NG <shihanng@gmail.com>
2021-01-16 19:23:06 +0900

    webpack: Add ProgressPlugin and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = ({ mode }) => {
   return {
     output: { filename: "bundle.js" },
     mode,
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
   };
 };
