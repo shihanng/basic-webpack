6076d8c973346b57fe94d00659b3dada79739ffd
Shi Han NG <shihanng@gmail.com>
2021-01-17 08:45:05 +0900

    Make multiple webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const modeConfig = (env) => require(`./build-utils/webpack.${env}`)(env);
+const { merge } = require("webpack-merge");
 
-module.exports = ({ mode }) => {
-  return {
-    output: { filename: "bundle.js" },
-    mode,
-    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
-  };
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  return merge(
+    {
+      output: { filename: "bundle.js" },
+      mode,
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()],
+    },
+    modeConfig(mode)
+  );
 };
